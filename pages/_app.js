858b94d721af0c3c5067d1cad4baf4b7e64ee702
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-08-22 07:26:58 +0430

    provider added

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import "antd/dist/antd.css";
 import "../styles/globals.css";
 
+import store from "../src/store";
+import { Provider } from "react-redux";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
