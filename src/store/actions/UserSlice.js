b3f0416a744c76afb55de38c4baa954206988ea6
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-08-22 07:35:31 +0430

    local storage

--- src/store/actions/UserSlice.js
+++ src/store/actions/UserSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import { saveSecure, getValueFor } from "../secure";
+import { saveSecure, getValueFor } from "../localStore";
 
 import { formRequest } from "./../../api";
 
