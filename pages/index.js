4c11ebf1ae8c76d9e0e0e274aff88c8dbb1c23a9
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-08-23 09:33:12 +0430

    login

--- pages/index.js
+++ pages/index.js
@@ -14,35 +14,35 @@ import NavBar from "../src/components/NavBar";
 export default function Home() {
 
 
-  const topics = ["First topic", "Second topic", "Third topic"];
-  const [contentIndex, setContentIndex] = useState(0);
-  const [selectedKey, setSelectedKey] = useState("0");
+  // const topics = ["First topic", "Second topic", "Third topic"];
+  // const [contentIndex, setContentIndex] = useState(0);
+  // const [selectedKey, setSelectedKey] = useState("0");
 
-  const changeSelectedKey = (event) => {
-    const key = event.key;
-    setSelectedKey(key);
-    setContentIndex(+key);
-  };
+  // const changeSelectedKey = (event) => {
+  //   const key = event.key;
+  //   setSelectedKey(key);
+  //   setContentIndex(+key);
+  // };
 
-  const Menu = (
-    <TopicMenu
-      topics={topics}
-      selectedKey={selectedKey}
-      changeSelectedKey={changeSelectedKey}
-    />
-  );
+  // const Menu = (
+  //   <TopicMenu
+  //     topics={topics}
+  //     selectedKey={selectedKey}
+  //     changeSelectedKey={changeSelectedKey}
+  //   />
+  // );
 
-  return (
-    <div className="App">
-      <NavBar menu={Menu} />
-      <Layout>
-        <SideBar menu={Menu} />
-        <Layout.Content className="content">
-          {topics[contentIndex]}
-        </Layout.Content>
-      </Layout>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <NavBar menu={Menu} />
+  //     <Layout>
+  //       <SideBar menu={Menu} />
+  //       <Layout.Content className="content">
+  //         {topics[contentIndex]}
+  //       </Layout.Content>
+  //     </Layout>
+  //   </div>
+  // );
 
 
   // above is test
