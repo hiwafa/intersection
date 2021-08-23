adcf20dbd1eb0e9d661e3d491da28e1966443c46
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-08-23 07:47:44 +0430

    navbar bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Drawer, Button } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
+import "../../styles/NavBar.module.css";
+import logo from "../../public/favicon.ico";
+
+
+const NavBar = ({ menu }) => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <nav className="navbar">
+      <Button
+        className="menu"
+        type="primary"
+        icon={<MenuOutlined />}
+        onClick={() => setVisible(true)}
+      />
+      <Drawer
+        title="Topics"
+        placement="left"
+        onClick={() => setVisible(false)}
+        onClose={() => setVisible(false)}
+        visible={visible}
+      > 
+        {menu}
+     </Drawer>
+     <a href="/"><img src={logo} className="logo" alt="logo" /></a>     
+    </nav>
+  );
+};
+export default NavBar;
