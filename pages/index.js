909733bfe70e3e73bd7c30ab650b6ed9a7bf61f5
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-08-23 08:04:10 +0430

    menu added

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,49 @@ import styles from '../styles/Home.module.css';
 import Login from "../src/components/Login";
 import { isLoggedIn, loadCredential } from '../src/store/actions/UserSlice';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
+import { Layout } from "antd";
+import TopicMenu from "../src/components/Menu";
+import SideBar from "../src/components/SideBar";
+import NavBar from "../src/components/NavBar";
 
 export default function Home() {
+
+
+  const topics = ["First topic", "Second topic", "Third topic"];
+  const [contentIndex, setContentIndex] = useState(0);
+  const [selectedKey, setSelectedKey] = useState("0");
+
+  const changeSelectedKey = (event) => {
+    const key = event.key;
+    setSelectedKey(key);
+    setContentIndex(+key);
+  };
+
+  const Menu = (
+    <TopicMenu
+      topics={topics}
+      selectedKey={selectedKey}
+      changeSelectedKey={changeSelectedKey}
+    />
+  );
+
+  return (
+    <div className="App">
+      <NavBar menu={Menu} />
+      <Layout>
+        <SideBar menu={Menu} />
+        <Layout.Content className="content">
+          {topics[contentIndex]}
+        </Layout.Content>
+      </Layout>
+    </div>
+  );
+
+
+  // above is test
 
   const checkLogin = useSelector(isLoggedIn);
   const dispatch = useDispatch();
