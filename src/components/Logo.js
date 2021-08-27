6fa6064f1062e300b31de7fa78844ecac10df821
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-08-27 22:11:53 +0430

    add menu

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from '/favicon.ico';
 import Image from "next/image"
-import { Row } from 'simple-flexbox';
+import { Row } from 'antd';
 import styles from '../../styles/Logo.module.css';
 
 
 const Logo = ()=> {
 
     return (
-        <Row className={styles.container} horizontal="center" vertical="center">
+        <Row className={styles.container}>
             <Image src={logo} />
             <span className={styles.title}>Intersection</span>
         </Row>
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,13 +1,47 @@
+import React from 'react';
+import { bool, func, string } from 'prop-types';
+import { Row } from 'simple-flexbox';
+import { StyleSheet, css } from 'aphrodite';
 
+const styles = {
+    activeBar: {
+        height: 56, width: 3,
+        backgroundColor: '#DDE2FF',
+        position: 'absolute', left: 0
+    },
+    activeContainer: {
+        backgroundColor: 'rgba(221,226,255, 0.08)'
+    },
+    activeTitle: { color: '#DDE2FF' },
+    container: {
+        height: 56,
+        cursor: 'pointer', ':hover': {
+            backgroundColor: 'rgba(221,226,255, 0.08)'
+        },
+        paddingLeft: 32,
+        paddingRight: 32
+    },
+    title: {
+        fontFamily: 'Muli',
+        fontSize: 16,
+        lineHeight: '20px',
+        letterSpacing: '0.2px',
+        color: '#A4A6B3',
+        marginLeft: 24
+    }
+}
 
 const  MenuItem = ({ active, icon, title, ...otherProps }) => {
-    
+
     const Icon = icon;
+    const conStyle = active ? {...styles.container, ...styles.activeContainer} : styles.container;
+    const titStyle = active ? {...styles.title, ...styles.activeTitle} : styles.title;
+
     return (
-        <Row className={css(styles.container, active && styles.activeContainer)} vertical="center" {...otherProps}>
-            {active && <div className={css(styles.activeBar)}></div>}
+        <Row className={conStyle} vertical="center" {...otherProps}>
+            {active && <div className={styles.activeBar}></div>}
             <Icon fill={active && "#DDE2FF"} opacity={!active && "0.4"} />
-            <span className={css(styles.title, active && styles.activeTitle)}>{title}</span>
+            <span className={titStyle}>{title}</span>
         </Row>
     );
 }
