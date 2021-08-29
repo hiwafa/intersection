53472da3544e27ee11173b4eeb2d12d17b2f4f59
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-08-29 09:27:52 +0430

    tabs created

--- pages/admin.js
+++ pages/admin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import GoogleMapReact from 'google-map-react';
 import { useRouter } from "next/router";
 
@@ -33,6 +33,17 @@ const config = {
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
+const getContent = tab => {
+
+  switch(tab){
+    case "tab1": return <Bubble {...config} />;
+    case "tab2": return <div>content 2</div>;
+    case "tab3": return <div>content 3</div>;
+    default: return null;
+  }
+
+}
+
 const defaultProps = {
   center: {
     lat: 10.99835602,
@@ -42,6 +53,8 @@ const defaultProps = {
 };
 
 const Admin = () => {
+
+  const [tab, setTab] = useState("tab1");
 
   return (
     <Layout>
@@ -60,15 +73,15 @@ const Admin = () => {
       </div>
       <Header style={{ width: '100%', backgroundColor: '#fff' }}>
         <div className="logo" />
-        <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">Descriptive Statistics</Menu.Item>
-          <Menu.Item key="2">Crash Data</Menu.Item>
-          <Menu.Item key="3">Intersection Inventory</Menu.Item>
+        <Menu theme="light" mode="horizontal" defaultSelectedKeys={['tab1']}>
+          <Menu.Item key="tab1" onClick={()=> setTab("tab1")}>Descriptive Statistics</Menu.Item>
+          <Menu.Item key="tab2" onClick={()=> setTab("tab2")}>Crash Data</Menu.Item>
+          <Menu.Item key="tab3" onClick={()=> setTab("tab3")}>Intersection Inventory</Menu.Item>
         </Menu>
       </Header>
       <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
         <div className="site-layout-background" style={{ minHeight: 380 }}>
-          <Bubble {...config} />
+          {getContent(tab)}
         </div>
       </Content>
     </Layout>
