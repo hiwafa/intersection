4d2ca2d99ee15c5ecf5457427caf71063d2f86de
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-08-29 07:43:23 +0430

    map installed

--- pages/admin.js
+++ pages/admin.js
@@ -1,12 +1,35 @@
+import React from "react";
+import GoogleMapReact from 'google-map-react';
 import { useRouter } from "next/router";
 
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+const defaultProps = {
+  center: {
+    lat: 10.99835602,
+    lng: 77.01502627
+  },
+  zoom: 11
+};
 
 const Admin = () => {
 
   return (
-   <div>Admin</div>
+    <div style={{ height: '40vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
   );
-
 }
 
 export default Admin;
