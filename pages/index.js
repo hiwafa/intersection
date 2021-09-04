b1042879fa79026d753e2849b18443ffcb5153f6
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-09-04 23:19:14 +0430

    responsive

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+import { Layout } from 'antd';
+import { useRef } from 'react';
+const { Header, Footer, Sider, Content } = Layout;
+
+
 const Home = () => {
+
+  const ref = useRef();
+
+  console.log("ref.current.offsetWidth", ref.current)
 
   return (
     <div className={styles.container}>
@@ -11,9 +20,20 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        Welcome you logged in
-      </main>
+      {/* <main className={styles.main}> */}
+
+        <div className={styles.row}>
+          <div className={styles.column} style={{backgroundColor: 'green'}}>
+            <h2>Column 1</h2>
+            <p>Some text..</p>
+          </div>
+          <div className={styles.column} style={{backgroundColor: 'blue'}}>
+            <h2>Column 2</h2>
+            <p>Some text..</p>
+          </div>
+        </div>
+
+      {/* </main> */}
     </div>
   );
 
