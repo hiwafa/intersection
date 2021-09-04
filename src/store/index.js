9f425b45cc52aee3ee466a5ea5acb437e99f0131
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-09-05 02:19:43 +0430

    added query for intersections

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-
+import { setupListeners } from '@reduxjs/toolkit/query';
 import userReducer from "./actions/UserSlice";
+import { myAPI } from "./query";
 
 const allReducers = {
     user: userReducer
 };
 
-export default configureStore({
-    reducer: allReducers
+const store = configureStore({
+    reducer: allReducers,
+    middleware: gdm => gdm().concat(myAPI.middleware)
 });
+
+
+export default store;
+
+setupListeners(store.dispatch);
