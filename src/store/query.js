e9de42c164fe1621a4b1ff37e80794f5fc67560f
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-09-05 09:09:04 +0430

    formated

--- src/store/query.js
+++ src/store/query.js
@@ -1,17 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseUrl = process.env.BASE_URL;
-const JWT_TOKEN = process.env.JWT_TOKEN;
+
+const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.JWT_TOKEN}`
+}
 
 export const intersectionAPI = createApi({
     reducerPath: "intersectionAPI",
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: builder => ({
         getIntersections: builder.query({
-            query: name => ({url: name, method: 'GET', headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${JWT_TOKEN}`
-            }})
+            query: name => ({url: name, method: 'GET', headers})
         })
     })
 });
