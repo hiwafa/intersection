b88438e8e1d67c907f1a9e1b00ddf11a52884925
hekmatnajafi <najafihekmat@gmail.com>
2021-09-10 23:28:34 +0430

    added functionlity to redirect user to create project by link

--- pages/projects.js
+++ pages/projects.js
@@ -1,11 +1,11 @@
-import react, { useState } from "react"
+import react, { useEffect, useState } from "react"
 import styled from "styled-components"
 import {Button, Row, Col} from "antd"
 import SearchProject from "../src/components/searchProject"
 import CreateProject from "../src/components/createProject"
 import ProjectDetails from "../src/components/projectDetails"
 import EditProject from "../src/components/editProject"
-
+import {useQueryParam} from "../src/utils/useQueryParam"
 import { PlusCircleOutlined } from '@ant-design/icons';
 import {PageTitle} from "../src/components/styleds"
 const ButtonContainer = styled.div`
@@ -17,10 +17,16 @@ function Projects(){
     const [showDetails, setShowDetails] = useState(false)
     const [section,setSection] = useState("")
     const [project, setProject] = useState("")
+    const queryParam = useQueryParam();
     const handleClick = (status) => {
-        console.log("called")
         setSearch(status)
     }
+    useEffect(()=>{
+        if(queryParam?.create && queryParam?.create == 1)
+        {
+            setSearch(false)
+        }
+    })
     return <>
         {showDetails ?
          (section && section === "edit" ? <EditProject project={project} setShowDetails={setShowDetails} /> :<ProjectDetails project={project} setShowDetails={setShowDetails} />) : 
