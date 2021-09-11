71e505b3ef4f68505748e0066546ba998072d0bc
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-09-11 10:30:02 +0430

    fixed selected key

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -23,11 +23,13 @@ const LayoutCom = ({ children }) => {
     const router = useRouter();
     let padname = router.pathname;
 
-    
+    console.log("path::::1", padname);
 
     if (padname === "/") padname = "home";
-    if (padname === "/projects/create") padname = "projects";
+    else if (padname === "/projects/create") padname = "projects";
     else padname = padname.substring(1);
+
+    console.log("path::::2", padname);
 
     const onLogout = async ()=> {
         try {
