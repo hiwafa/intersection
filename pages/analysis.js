f2ba7ec6c154e93f74e235e0212557febc4935f8
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-09-12 20:42:02 +0430

    Formated

--- pages/analysis.js
+++ pages/analysis.js
@@ -51,9 +51,11 @@ function Analys() {
 
                 let check4;
                 const check1 = i.INTERSECTION_TYPE === values.intersection;
-                const check2 =  values.crash === "All" ? true :
-                i.crash_intersections.some(v => v.SEVERITY === values.crash);
-                const check3 = i.crash_intersections.some(v => v.COLLISION_TYPE === values.collision);
+                const check2 = values.crash === "All" ? true : i.crash_intersections
+                    .some(v => v.SEVERITY === values.crash);
+
+                const check3 = i.crash_intersections
+                    .some(v => v.COLLISION_TYPE === values.collision);
 
                 if (values.from && values.to) {
 
@@ -76,7 +78,7 @@ function Analys() {
                 const check12 = values.intersection ? check1 : true;
                 const check22 = values.crash ? check2 : true;
                 const check32 = values.collision ? check3 : true;
-                
+
                 return check12 && check22 && check32 && check4;
             });
 
@@ -96,14 +98,14 @@ function Analys() {
                 </Col>
                 <Col flex={1} md span={12} className={styles.col2}>
 
-                    <Menu theme="light" style={{backgroundColor: '#f5f5f5'}} mode="horizontal" defaultSelectedKeys={['tab1']}>
+                    <Menu theme="light" style={{ backgroundColor: '#f5f5f5' }} mode="horizontal" defaultSelectedKeys={['tab1']}>
                         <Menu.Item key="tab1" onClick={() => setTab("tab1")}>Descriptive Statistics</Menu.Item>
                         <Menu.Item key="tab2" onClick={() => setTab("tab2")}>Crash Data</Menu.Item>
                         <Menu.Item key="tab3" onClick={() => setTab("tab3")}>Intersection Inventory</Menu.Item>
                     </Menu>
 
                     <Content className="site-layout" style={{ padding: '0 50px', marginTop: 20 }}>
-                        <StyledButton onClick={()=> push("projects/create")} style={{width: 120, marginBottom: 10}}>
+                        <StyledButton onClick={() => push("projects/create")} style={{ width: 120, marginBottom: 10 }}>
                             Add to projects
                         </StyledButton>
                         <div className="site-layout-background" style={{ minHeight: 380, width: '100%' }}>
