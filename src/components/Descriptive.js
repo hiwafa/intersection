0fd3046da86d9a5cebdef5f6375ef0f1fccc7f55
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-09-13 08:52:09 +0430

    Severity avoid duplicate and cents

--- src/components/Descriptive.js
+++ src/components/Descriptive.js
@@ -2,18 +2,38 @@ import react from "react";
 import { Row, Col } from 'antd';
 import styles from "../../styles/Analys.module.css";
 
+
+const getSeverity = arr => {
+    const counts = {};
+    arr.forEach(x => {
+        if(x.SEVERITY){
+            counts[x.SEVERITY] = (counts[x.SEVERITY] || 0) + 1;
+        }
+    });
+    
+    return Object.entries(counts);
+};
+
 const Descriptive = ({ inventory }) => {
     return (
         <div>
 
             <Row>
-                <Col span={24} className={styles.headCol}>Severity</Col>
+                <Col span={8} className={styles.headCol}>Severity</Col>
+                <Col span={8} className={styles.headCol}>Number</Col>
+                <Col span={8} className={styles.headCol}>Percent</Col>
             </Row>
-            {inventory && inventory.crash_intersections && inventory.crash_intersections.map(crash => {
+            {inventory && inventory.crash_intersections && getSeverity(inventory.crash_intersections).map(crash => {
                 return (
                     <Row key={crash.id}>
-                        <Col span={24} className={styles.cell}>
-                            {crash.SEVERITY}
+                        <Col span={8} className={styles.cell}>
+                            {crash[0]}
+                        </Col>
+                        <Col span={8} className={styles.cell}>
+                            {crash[1]}
+                        </Col>
+                        <Col span={8} className={styles.cell}>
+                            {100/crash[1]}%
                         </Col>
                     </Row>
                 );
