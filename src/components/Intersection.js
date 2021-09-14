d2953c17b54a8a094826a2c809f8909a05c3a23f
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-09-14 08:19:59 +0430

    hey dear

--- src/components/Intersection.js
+++ src/components/Intersection.js
@@ -9,6 +9,7 @@ const IntersectionInventory = ({ inventory }) => (
             <Col span={6} className={styles.headCol}>Intersection Type</Col>
             <Col span={6} className={styles.headCol}>No. of Approaches</Col>
             <Col span={6} className={styles.headCol}>AADT</Col>
+            <Col span={6} className={styles.headCol}>AADT GROWTH FACTOR</Col>
         </Row>
 
         <Row className={styles.row}>
@@ -25,6 +26,11 @@ const IntersectionInventory = ({ inventory }) => (
             <Col span={6} className={styles.cell}>
                 <Typography.Text ellipsis={true}>
                     {inventory && inventory.AADT}
+                </Typography.Text>
+            </Col>
+            <Col span={6} className={styles.cell}>
+                <Typography.Text ellipsis={true}>
+                    3
                 </Typography.Text>
             </Col>
         </Row>
