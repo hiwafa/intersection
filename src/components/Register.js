90bf1574507f8a92fac70a1d3df1b07985080472
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-09-19 02:28:30 +0430

    default password

--- src/components/Register.js
+++ src/components/Register.js
@@ -12,6 +12,14 @@ const spinStyle = css({
   '.ant-spin-dot-item': { backgroundColor: `#fff;` }
 });
 
+function generateUID() {
+  var firstPart = (Math.random() * 46656) | 0;
+  var secondPart = (Math.random() * 46656) | 0;
+  firstPart = ("000" + firstPart.toString(36)).slice(-3);
+  secondPart = ("000" + secondPart.toString(36)).slice(-3);
+  return firstPart + secondPart;
+}
+
 const Register = ({ setVisible, record }) => {
 
   const formRef = useRef();
@@ -102,6 +110,7 @@ const Register = ({ setVisible, record }) => {
               message: "Please input your password!",
             },
           ]}
+          initialValue={!record ? generateUID() : ""}
         >
           <Input.Password
             prefix={<LockOutlined className="site-form-item-icon" />}
