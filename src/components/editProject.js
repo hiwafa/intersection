e7eea52b57d0e3a112d3bddd281a37e065b3398e
hekmatnajafi <najafihekmat@gmail.com>
2021-09-19 22:51:36 +0430

    fixed file upload error

--- src/components/editProject.js
+++ src/components/editProject.js
@@ -102,7 +102,7 @@ function EditProject ({project, setShowDetails}){
         }
 
       try{
-        const upload = await axios.post(`${BASE_URL}upload`,files, {
+        const upload = await axios.post(`${BASE_URL}/upload`,files, {
           headers: {
             'Accept': 'application/json'
             },
