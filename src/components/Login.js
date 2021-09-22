c73b7437290847e1b1a0d944b3019ed61826f549
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-09-22 08:21:03 +0430

    added loading indicator to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,29 @@
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Spin } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import styles from "../../styles/Login.module.css";
 import { signin } from '../store/actions/UserSlice';
 import { useDispatch } from 'react-redux';
-import Link from "next/link";
-
 import { StyledButton } from './styleds';
+import { css } from '@emotion/css';
+
+const spinStyle = css({
+    '.ant-spin-dot-item': { backgroundColor: `#fff;` }
+  });
 
 const Login = () => {
 
     const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
 
     const onFinish = async (values) => {
         try {
 
-            const { payload } = await dispatch(signin(values));
+            if (loading === true) return;
 
-            console.log("login result: ", payload);
+            setLoading(true);
+
+            const { payload } = await dispatch(signin(values));
+            setLoading(false);
 
         } catch (err) {
             console.log('ERR:Login:onFinish ', err);
@@ -25,56 +32,58 @@ const Login = () => {
 
     return (
         <>
-        <Form
-            name="normal_login"
-            className="login-form"
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-        >
-
-            <Form.Item
-                name="username"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your username!',
-                    },
-                ]}
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
             >
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-            </Form.Item>
 
-            <Form.Item
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Password!',
-                    },
-                ]}
-            >
-                <Input
-                    prefix={<LockOutlined className="site-form-item-icon" />}
-                    type="password"
-                    placeholder="Password"
-                />
-            </Form.Item>
+                <Form.Item
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
+                >
+                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                </Form.Item>
 
-            <Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Password!',
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={<LockOutlined className="site-form-item-icon" />}
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Item>
 
-                {/* <Link target="_blank" href="/reset"> */}
+                <Form.Item>
+
+                    {/* <Link target="_blank" href="/reset"> */}
                     <a className={styles.forgetPass}>Forgot password?</a>
-                {/* </Link> */}
-            </Form.Item>
+                    {/* </Link> */}
+                </Form.Item>
 
-            <Form.Item>
-                <StyledButton type="submit">
-                    Log in
-                </StyledButton>
-            </Form.Item>
-        </Form>
+                <Form.Item>
+                    <StyledButton type="submit">
+                        {
+                            loading ? <Spin size="small" className={spinStyle} /> : "Log In"
+                        }
+                    </StyledButton>
+                </Form.Item>
+            </Form>
         </>
     );
 };
