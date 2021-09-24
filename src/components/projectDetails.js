309891380f4de5a96cf1fac0cfe36510e44133c3
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-09-24 17:48:54 +0430

    name updated

--- src/components/projectDetails.js
+++ src/components/projectDetails.js
@@ -332,7 +332,7 @@ function ProjectDetails({ project, crashCostList, setShowDetails, intersection }
       >
         Download Project Details
       </ThemButton>
-      {project.projectFile?.url && <a href={`${BASE_URL}${project.projectFile?.url}`}><Button type={"primary"} icon={<DownloadOutlined />} htmlType={"button"} size={"medium"} className={"downloadButton"}>Download Project File</Button></a>}
+      {project.projectFile?.url && <a href={`${BASE_URL}${project.projectFile?.url}`}><Button type={"primary"} icon={<DownloadOutlined />} htmlType={"button"} size={"medium"} className={"downloadButton"}>Download Attachments</Button></a>}
     </PageTitle>
 
     <TableContainer style={{ maxWidth: "450px", margin: "auto" }}>
