42b22c7784da3eb881504a8d3496dafa0261291b
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-10-02 00:41:54 +0430

    fixing calculations update 11 fix

--- src/components/createProject.js
+++ src/components/createProject.js
@@ -12,10 +12,15 @@ import 'moment/locale/zh-cn';
 
 import { useGetIntersectionsQuery } from "../store/query";
 
-const firstCounter = ({ crashes, CRASH_COUNT, AADT }) => {
+const firstCounter = ({
+    crashes, CRASH_COUNT, AADT,
+    CRASH_END_DATE, CRASH_START_DATE
+}) => {
 
     let a = 0, b = 0, c = 0, pdo = 0, epdo = 0,
         injuries = 0, fatalities = 0, crashRate = 0;
+
+    if (!(CRASH_END_DATE && CRASH_START_DATE)) return false;
 
     let endDate = moment(CRASH_END_DATE);
     let startDate = moment(CRASH_START_DATE);
@@ -34,21 +39,14 @@ const firstCounter = ({ crashes, CRASH_COUNT, AADT }) => {
 
     epdo = 542 * fatalities + 11 * injuries + 1 * pdo;
 
-    return { a, b, c, injuries, fatalities, pdo, epdo, crashRate };
+    return { a, b, c, injuries, fatalities, pdo, epdo, crashRate, years };
 };
 
 const getCalculatedData = ({
-    CRASH_END_DATE, CRASH_START_DATE,
     treatments, AADT_GROWTH_FACTOR,
     fCrashCost, iCrashCost, pCrashCost,
-    injuries, fatalities, pdo,
+    injuries, fatalities, pdo, years,
 }) => {
-
-    if (!(CRASH_END_DATE && CRASH_START_DATE)) return false;
-
-    let endDate = moment(CRASH_END_DATE);
-    let startDate = moment(CRASH_START_DATE);
-    let years = (endDate.diff(startDate, "days") / 365).toFixed(5);
 
     let crf = 1, EUAC = 0.0, EUAB = 0.0;
     treatments && treatments.forEach(treat => {
